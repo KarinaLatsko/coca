73c347498e52fd97b0bc96e56e8865b0a7b17e05
Karina <sibflower@yandex.ru>
2024-11-08 15:48:32 +0700

    Changed file theme.js

--- src/js/components/theme.js
+++ src/js/components/theme.js
@@ -1,12 +1,24 @@
 export const useTheme = () => {
-  const themeTarget = document.querySelector('[data-theme]');
-  const themeSwitcher = document.querySelector('[data-theme-switcher]');
+  const themeTarget = document.querySelector("[data-theme]");
+  const themeSwitcher = document.querySelector("[data-theme-switcher]");
 
-  themeSwitcher.addEventListener('click', () => {
-    const themeTargetDataset = themeTarget.dataset;
+  const themeTargetDataset = themeTarget.dataset;
+
+  const theme = localStorage.getItem('theme') || 'light';
+  themeTargetDataset.theme = theme;
+
+  if (theme === 'dark') {
+    themeSwitcher.checked = true;
+  }
+
+  themeSwitcher.addEventListener("click", () => {
     
-    themeTargetDataset.theme == 'light'
-    ? (themeTargetDataset.theme = 'dark')
-    : (themeTargetDataset.theme = 'light');
+    if (themeTargetDataset.theme === "light") {
+      themeTargetDataset.theme = "dark";
+      localStorage.setItem("theme", "dark");
+    } else {
+      themeTargetDataset.theme = "light";
+      localStorage.setItem("theme", "light");
+    }
   });
 };
